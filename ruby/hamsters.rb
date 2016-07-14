8ac2ec2ee22a207fb07689bc74beaac0e1cde47d
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-14 13:59:01 -0400

    Make it so it prints data to console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,7 +4,7 @@ name = gets.chomp
 puts "What's your preferred volume level? (1-10)"
 volume_level = gets.chomp
 until volume_level.to_i > 0 && volume_level.to_i < 10
-  puts "What's your preferred volume level? (1-10)"
+  puts "Invalid input. What's your preferred volume level? (1-10)"
   volume_level = gets.chomp
 end
 
@@ -14,8 +14,17 @@ fur_color = gets.chomp
 puts "Is this hamster a good candidate for adoption? (y/n)"
 adoption_candidate = gets.chomp
 
+
 puts "Around what age?"
 age = gets.chomp
 if age == ""
   age = nil
+  puts "Unknown"
 end
+age.to_i
+
+puts "Name = #{name}"
+puts "Preferred volume level = #{volume_level}"
+puts "Favorite fur color = #{fur_color}"
+puts "Good candidate for adoption = #{adoption_candidate}"
+puts "Age = #{age}"

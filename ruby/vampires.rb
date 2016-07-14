83ba38b47b2ff3387bab10fe95f56bef2a1e262b
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-14 14:23:05 -0400

    Creat questionnaire for applicants

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+vampire_name = gets.chomp
+
+puts "How old are you? What year were you born?"
+vampire_age = gets.chomp
+vampire_age.to_i
+vampire_birth_year = gets.chomp
+vampire_birth_year.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+health_insuarance = gets.chomp

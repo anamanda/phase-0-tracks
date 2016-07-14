f8ba94f2e678a597cc9cb2cc6c2578f722c456a8
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-14 15:59:48 -0400

    Add loop to process multiple applicants

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,27 +1,38 @@
-puts "What is your name?"
-vampire_name = gets.chomp
+puts "How many employees will be processed?"
+number_of_employees = gets.chomp
 
-puts "How old are you? What year were you born?"
-puts "Age:"
-vampire_age = gets.chomp
-puts "Birth year:"
-vampire_birth_year = gets.chomp
+process_count = 0
+
+until process_count == number_of_employees.to_i
+
+  puts "What is your name?"
+  vampire_name = gets.chomp
+
+  puts "How old are you? What year were you born?"
+  puts "Age:"
+  vampire_age = gets.chomp
+  puts "Birth year:"
+  vampire_birth_year = gets.chomp
 
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic_bread = gets.chomp
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+  garlic_bread = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-health_insurance = gets.chomp
+  puts "Would you like to enroll in the company's health insurance? (y/n)"
+  health_insurance = gets.chomp
 
-if vampire_age.to_i == (2016 - vampire_birth_year.to_i) && (garlic_bread =="y" || health_insurance == "y")
-  puts "Probably not a vampire."
-elsif vampire_age.to_i != (2016 - vampire_birth_year.to_i) && (garlic_bread == "n" || health_insurance == "n")
-  puts "Probably a vampire."
-elsif vampire_age.to_i != (2016 - vampire_birth_year.to_i) && garlic_bread == "n" && health_insurance == "n"
-  puts "Almost certainly a vampire."
-elsif vampire_name.to_i == "Drake Cula" || vampire_name == "Tu Fang"
-  puts "Definitely a vampire."
-else
-  puts "Results inconclusive."
+  if vampire_age.to_i == (2016 - vampire_birth_year.to_i) && (garlic_bread =="y" || health_insurance == "y")
+    puts "Probably not a vampire."
+  elsif vampire_age.to_i != (2016 - vampire_birth_year.to_i) && (garlic_bread == "n" || health_insurance == "n")
+    puts "Probably a vampire."
+  elsif vampire_age.to_i != (2016 - vampire_birth_year.to_i) && garlic_bread == "n" && health_insurance == "n"
+    puts "Almost certainly a vampire."
+  elsif vampire_name.to_i == "Drake Cula" || vampire_name == "Tu Fang"
+    puts "Definitely a vampire."
+  else
+    puts "Results inconclusive."
+  end
+
+  process_count += 1
+
 end

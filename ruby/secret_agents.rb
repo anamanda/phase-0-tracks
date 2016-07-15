ae0ecd38405c480fef9714343d3b9c3b8f214c6f
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-15 10:52:45 -0400

    Add pseudocode for encryption method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,15 @@
+# create counter variable to loop through encryption
+
+# start-variable that has beginning input
+
+# create loop that goes through each position and move it one position forward and save it
+
+  # if start-variable[counter] is " "
+    # start-variable[counter] = start-variable[counter]
+
+  # else
+    # go to the next character and save that
+  
+  # loop again, go to next position, repeat until length of start-variable is being reached
+
+

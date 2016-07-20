326b2ee696714897ecba60ae53a765c58d4e0ea7
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-20 19:21:57 -0400

    Add nested data structure of a band makeup

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,20 @@
+band {
+  drummer: ['drumsticks',
+            'kick drums'
+            'snare drum'
+            'cymbals'
+          ]
+
+  guitarist: {
+    guitarist1: ['electric guitar',
+                 'pick'
+               ]
+    guitarist2: ['acoustic guitar'
+                 'pick'
+               ]
+  }
+
+  singer: ['microphone', 'microphone stand', 'vocal FX pedal']
+
+  bassist: ['bass guitar']
+}

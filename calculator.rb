1d57b3f0ce96fe195b55ecc653e82447cc011efe
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-25 12:07:12 -0400

    Edit code so it can take multiple-digit numbers

--- calculator.rb
+++ calculator.rb
@@ -13,16 +13,28 @@ end
 # p calculate(4, '*', 5) # should equal 20
 # p calculate(10, '/', 5) # should equal 2
 
+
 # prompt user to enter a calculation
-puts "Please enter a calculation you would like to perform (integer + integer):"
+puts "Please enter a calculation you would like to perform (integer + operation + integer):"
 user_input = gets.chomp
 
-# their input at position 0 (convert to integer) should be the first integer
-# their input at position 2 should be the operation string
-# their input at position 4 (convert to integer) should be the second integer
-user_integer1 = user_input[0].to_i
-user_operation = user_input[2]
-user_integer2 = user_input[4].to_i
+# scan the user input for the first integer
+user_integer1 = user_input.scan(/\d+/).first.to_i
+
+# remove user_integer1 from user_input
+user_input.delete!(user_integer1.to_s)
+
+# scan the user input for the second integer
+user_integer2 = user_input.scan(/\d+/).last.to_i
+
+# remove user_integer2 from user_input
+user_input.delete!(user_integer2.to_s)
+
+# remove whitespace around remaining string, which is ' operator '
+user_input.delete!(' ')
+
+# set user_operation to remaining string
+user_operation = user_input
 
 # print result of calculate method with user input
 p calculate(user_integer1, user_operation, user_integer2)

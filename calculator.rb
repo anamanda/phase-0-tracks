4c9d750934d6835b557d900acad23d25b61d6e9c
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-25 11:40:45 -0400

    Add driver code that tests all four operations and prints the results

--- calculator.rb
+++ calculator.rb
@@ -7,4 +7,8 @@ def calculate(integer1, operator_string, integer2)
   integer1.send(operator_string, integer2)
 end
 
-p calculate(1, '+', 4)
+# driver code that will test all four operators
+p calculate(1, '+', 4) # should equal 5
+p calculate(4, '-', 1) # should equal 3
+p calculate(4, '*', 5) # should equal 20
+p calculate(10, '/', 5) # should equal 2

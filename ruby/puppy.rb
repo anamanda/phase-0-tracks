588e2b4df28b4f7e4371bb86612c51230fd6c587
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-26 13:42:52 -0400

    Create puppy species class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,15 +1,3 @@
 # SPECIES --------------------
 # Puppius Adorableus
 
-# CHARACTERISTICS --------------------
-# Paw count: 4
-# Fur type: varies
-# Color: varies
-# Barks: yes
-# Size: varies
-
-# BEHAVIOR --------------------
-# Hug
-# Cuddle
-# Run
-

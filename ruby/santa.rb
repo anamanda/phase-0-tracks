841b05724477e51ad9535e5647510c8ff4c25b4d
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-28 12:51:14 -0400

    Add driver code to confirm initialization and methods work on Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,14 +8,25 @@ class Santa
     puts "Initializing Santa instance"
   end
 
-  # create speak instance method where Santa says something
+  # create speak instance method where Santa says something, return phrase
   def speak
-    puts "Ho, ho, ho! Haaaappy holidays!"
+    speak_phrase = "Ho, ho, ho! Haaaappy holidays!"
+    speak_phrase
   end
 
-  # create instance method that takes in a cookie type and Santa says that was a good cookie
+  # create instance method that takes in a cookie type and Santa says that was a good cookie, return phrase
   def eat_milk_and_cookies(cookie_type)
-    puts "That was a good #{cookie_type}!"
+    cookie_phrase = "That was a good #{cookie_type}!"
+    cookie_phrase
   end
 
 end
+
+# # Driver Code to test instance methods
+
+# # create a new Santa instance
+# first_santa = Santa.new
+
+# # call speak method on it and cookies and milk method on it
+# puts first_santa.speak
+# puts first_santa.eat_milk_and_cookies("snickerdoodle")

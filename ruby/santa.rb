f55b9b652402bc871749d030d70837c00453c91d
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-28 14:15:10 -0400

    Add gender, ethnicity, reindeer_ranking, and age attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,30 +3,46 @@
 # create Santa class
 class Santa
 
-  # create initialize method
-  def initialize
+  # create initialize method, add a gender and ethnicity parameter
+  def initialize(gender, ethnicity)
+
+    # print initialization sentence
     puts "Initializing Santa instance"
+
+    # create a gender attribute that is passed in to this method
+    @gender = gender
+
+    # create an ethnicity attribute that is passed in to this method
+    @ethnicity = ethnicity
+
+    # create a reindeer attribute that holds an array of reindeer names
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+    # create an age attribute that defaults to 0
+    @age = 0
+
+
   end
 
-  # create speak instance method where Santa says something, return phrase
+
+  # create speak instance method where Santa says something
   def speak
-    speak_phrase = "Ho, ho, ho! Haaaappy holidays!"
-    speak_phrase
+    puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
-  # create instance method that takes in a cookie type and Santa says that was a good cookie, return phrase
+
+  # create instance method that takes in a cookie type and Santa says that was a good cookie
   def eat_milk_and_cookies(cookie_type)
-    cookie_phrase = "That was a good #{cookie_type}!"
-    cookie_phrase
+    puts"That was a good #{cookie_type}!"
   end
 
 end
 
-# # Driver Code to test instance methods
+# Driver Code to test instance methods
 
-# # create a new Santa instance
-# first_santa = Santa.new
+# create a new Santa instance
+first_santa = Santa.new
 
-# # call speak method on it and cookies and milk method on it
-# puts first_santa.speak
-# puts first_santa.eat_milk_and_cookies("snickerdoodle")
+# call speak method on it and cookies and milk method on it
+first_santa.speak
+first_santa.eat_milk_and_cookies("snickerdoodle")

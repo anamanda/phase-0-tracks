72d984011c5e19d488634016ede13e88d22a335f
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-29 18:41:48 -0400

    Create class method to add item, add comment code

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,7 +1,16 @@
 class TodoList
- attr_reader :get_items
 
- def initialize(get_items)
-   @get_items = get_items
- end
+  # create attr_reader for the get_items variable storing items on to do list
+  attr_reader :get_items
+
+  # create initialize method that takes get_items variable, set @get_items to get_items
+  def initialize(get_items)
+    @get_items = get_items
+  end
+
+  # create add item method to add item
+  def add_item(item)
+    @get_items << item
+  end
+
 end

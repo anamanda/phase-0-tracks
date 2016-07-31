e7503847a0d14cf91b394b6fb89c80f590b57bd1
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-31 13:26:31 -0400

    Create mixin version of Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,19 +1,28 @@
 # 6.4 - Modules
 
 
-# create module called Shout, define yell angrily and yell happily methods
+# # create module called Shout, define yell angrily and yell happily methods
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words + "!!!!!!" + " :) :) :)"
+#   end
+# end
+
+
+# # Driver Code for yell_angrily and yell_happily
+
+# p Shout.yell_angrily("This isn't working")
+# p Shout.yell_happily("Oh, wait, it is")
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "!!!!!!" + " :) :) :)"
-  end
 end
-
-
-# Driver Code for yell_angrily and yell_happily
-
-p Shout.yell_angrily("This isn't working")
-p Shout.yell_happily("Oh, wait, it is")

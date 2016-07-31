7b73d06707cbb8c7d6666cbb32e2c356e96c082e
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-31 13:09:20 -0400

    Add driver code for Shout module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -4,11 +4,16 @@
 # create module called Shout, define yell angrily and yell happily methods
 module Shout
   def self.yell_angrily(words)
-    words + "!!!" + ":("
+    words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
-    words + "!!!!!!" + ":) :) :)"
+  def self.yell_happily(words)
+    words + "!!!!!!" + " :) :) :)"
   end
 end
 
+
+# Driver Code for yell_angrily and yell_happily
+
+p Shout.yell_angrily("This isn't working")
+p Shout.yell_happily("Oh, wait, it is")

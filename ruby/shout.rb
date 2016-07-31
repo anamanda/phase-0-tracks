cec9bffbf5f4cca965cd60fd93d4ce06aac2a4d7
Amanda Steckler <amandajillsteckler@gmail.com>
2016-07-31 14:05:47 -0400

    Create Customer / Child classes and call yell_angrily / yell_happily methods on them

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -18,6 +18,8 @@
 # p Shout.yell_angrily("This isn't working")
 # p Shout.yell_happily("Oh, wait, it is")
 
+
+# create mixin Shout module
 module Shout
   def yell_angrily(words)
     words + "!!!" + " :("
@@ -25,4 +27,33 @@ module Shout
 
   def yell_happily(words)
     words + "!!!!!!" + " :) :) :)"
+  end
 end
+
+# create Customer class
+class Customer
+  include Shout
+end
+
+# create Child class
+class Child
+  include Shout
+end
+
+# Driver code for Customer and Child classes / Shout module
+# create new Customer / Child instances and call yell_angrily / yell_happily methods on them
+
+customer = Customer.new
+puts customer.yell_angrily("You got my order wrong")
+puts customer.yell_happily("Thanks for your help")
+
+child = Child.new
+puts child.yell_angrily("I don't want to eat my vegetables")
+puts  child.yell_happily("I love ice cream")
+
+
+
+
+
+
+

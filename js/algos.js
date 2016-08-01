bde3cbb3c558d2e7cd7b8e00465126738527f158
Amanda Steckler <amandajillsteckler@gmail.com>
2016-08-01 19:16:01 -0400

    Create longestWord function that returns longest word in an array

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,27 @@
 // write a function that takes has an array parameter
 // create an empty variable that will hold the longest word
 // loop through the array with a for loop, going from 0 to the end of the array
-// put the first word in the empty variable
-// if the next word is longer than the first word, replace it with the longer word
+// if the next word is longer than the previous word, replace it with the longer word
 // go through the whole array replacing smaller word with longer word
 
+
+// declare longestWord function that takes in an array
+function longestWord(wordArray) {
+
+  // create empty variable to hold the longest word
+  var longestWord = "";
+
+  // create a loop that goes through the array
+  // replace previous word with current word if current word is longer than previous word
+  for (i = 0; i < wordArray.length; i++) {
+    if (wordArray[i].length > longestWord.length)
+      longestWord = wordArray[i]
+  };
+
+  // return longestWord
+  return longestWord;
+};
+
+// Driver Code to test longestWord function
+console.log(longestWord(["amanda", "is", "my", "name"]))
+console.log(longestWord(["my", "name", "is", "amanda"]))

01a303856b0ad6661be1afae091c00a44119e27a
Amanda Steckler <amandajillsteckler@gmail.com>
2016-08-01 19:04:27 -0400

    Pseudocode algorithm to return longest word in array

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// write a function that takes has an array parameter
+// create an empty variable that will hold the longest word
+// loop through the array with a for loop, going from 0 to the end of the array
+// put the first word in the empty variable
+// if the next word is longer than the first word, replace it with the longer word
+// go through the whole array replacing smaller word with longer word
+

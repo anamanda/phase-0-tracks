f21f8619b9e2393083dc23a01f4d541bcd57f4e1
Amanda Steckler <amandajillsteckler@gmail.com>
2016-08-01 17:03:45 -0400

    Switch to driver / have data structures that match names and colors of horses

---
+++ js/data_structures.js
@@ -0,0 +1,17 @@
+var colors = ["blue", "orange", "green", "red"];
+var names = ["Smokey", "Patches", "Captain", "Trotty"];
+
+colors.push("purple");
+names.push("Maney");
+
+console.log(colors);
+console.log(names);
+
+var horses = {};
+for (var i = 0; i < colors.length; i++) {
+    horseNames = names[i];
+    horseColor = colors[i];
+    horses[horseNames] = horseColor;
+};
+
+console.log(horses);

1f4394d242e38222cbb9876757065b6692adbb9a
Amanda Steckler <amandajillsteckler@gmail.com>
2016-08-02 14:54:30 -0400

    Add function that takes in two objects and returns true if there is a property/value match

--- js/algos.js
+++ js/algos.js
@@ -40,3 +40,41 @@ console.log(longestWord(["i", "love", "coding", "lots"])); // should return codi
 // if match is equal to true, print match to the console
 // otherwise, print false to the console
 
+// declare 3 objects as test variables
+var object1 = {name: "Steve", age: 53, location: "USA"};
+var object2 = {name: "Sally", age: 54, location: "UK"};
+var object3 = {name: "Amanda", age: 24, location: "USA"}
+
+// declare function that takes in two objects to compare
+function objectMatch(obj1, obj2) {
+
+  // loop through obj1, and subsequently obj2 while looping through obj1
+  for (var prop in obj1) {
+    for (var key in obj2) {
+
+      // if the property and value of obj1 = property and value of obj2
+      // create a variable called match and set it equal to true
+      if (prop + obj1[prop] == key + obj2[key])
+        var match = true;
+    };
+  };
+
+  // if match == true, print match to the console
+  if (match == true) {
+    console.log(match)
+  }
+
+  // otherwise, print false to the console
+  else {
+    console.log(false)
+  };
+};
+
+// Driver Code
+objectMatch(object1, object2) // should return false
+objectMatch(object1, object3) // should return true
+
+
+
+
+

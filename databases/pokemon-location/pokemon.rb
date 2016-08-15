2c94f5e44aade2169d195bba406d2e927eeea569
Amanda Steckler <amandajillsteckler@gmail.com>
2016-08-14 20:43:11 -0400

    create pokemon_location database

--- databases/pokemon-location/pokemon.rb
+++ databases/pokemon-location/pokemon.rb
@@ -2,3 +2,16 @@
 require 'sqlite3'
 require 'faker'
 
+# create SQLite3 database
+pokemon_db = SQLite3::Database.new("pokemon_location.db")
+
+# create SQLite3 table
+# id, pokemon, cp, location
+create_pokemon_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS pokmeon (
+    id INTEGER PRIMARY KEY,
+    pokemon VARCHAR(255),
+    cp INT,
+    location VARCHAR(255)
+  )
+SQL

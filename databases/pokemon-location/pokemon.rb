2b97a531ff481ca75dd6b52921021d0d7a7c9c62
Amanda Steckler <amandajillsteckler@gmail.com>
2016-08-14 20:56:50 -0400

    create method for adding pokmeon, their cp, and their location, and add 10 pokemon

--- databases/pokemon-location/pokemon.rb
+++ databases/pokemon-location/pokemon.rb
@@ -16,5 +16,15 @@ create_pokemon_table = <<-SQL
   )
 SQL
 
+# create pokemon table
 pokemon_db.execute(create_pokemon_table)
 
+# create method for adding pokemon, cp, and location into table
+def create_pokemon(db, pokemon, cp, location)
+  db.execute("INSERT INTO pokemon (pokemon, cp, location) VALUES (?, ?, ?)", [pokemon, cp, location])
+end
+
+# create 10 pokemon
+10.times do
+  create_pokemon(pokemon_db, Faker::Pokemon.name, Faker::Number.between(1, 1000), Faker::Pokemon.location)
+end
